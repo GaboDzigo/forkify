3ed21c0346266e565c76fcb86825f59012c4c8b7
GaboDzigo <gabodzigo@gmail.com>
2024-03-03 19:12:06 +0200

    js

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -1,3 +1,9 @@
+import * as model from './model.js'
+import recipeView from './recipeView.js';
+
+import 'core-js/stable'
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -12,30 +18,26 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
-const showRecipe = async function () {
+
+
+const controlRecipes = async function () {
   try {
-    const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40'
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604691c37cdc054bc886'
-    );
-    const data = await res.json();
+    const id = window.location.hash.slice(1);
+    console.log(id);
 
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    if(!id) return;
+    recipeView.renderSpinner()
+    // 1) loading recipe
+   await model.loadRecipe(id);
 
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      tittle: recipe.tittle,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-    console.log(recipe);
+    // 2) rendering the recipe
+    recipeView.render(model.state.recipe);
   } catch (err) {
     alert(err);
   }
 };
-showRecipe();
+  
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes))
+
+// window.addEventListener('hashchange', controlRecipes)
+// window.addEventListener('load', controlRecipes)
